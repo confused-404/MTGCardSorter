4280bb573f67e9d52a8e1be628a3bdf397ce080a
Yves Kloeckner <hosta.gruen@gmail.com>
2024-02-28 22:54:15 -0700

    saving frames to files

--- src/camera.py
+++ src/camera.py
@@ -3,11 +3,17 @@ import cv2 as cv
 
 vid_capture = cv.VideoCapture(0)
 
+imgs = 0
+
 while True:
     try:
         b, frame = vid_capture.read();
-
+        
         cv.imshow("Frame", frame)
+
+        cv.imwrite("imgs\\frame" + str(imgs) + ".jpg", frame)
+
+        imgs += 1
 
         key = cv.waitKey(1)
 
