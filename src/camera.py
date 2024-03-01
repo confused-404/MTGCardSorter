8d4914f0711326d4ca2c723be4ba9606832ffe18
Yves Kloeckner <hosta.gruen@gmail.com>
2024-02-29 19:55:16 -0700

    only camera

--- src/camera.py
+++ src/camera.py
@@ -6,17 +6,18 @@ vid_capture = cv.VideoCapture(0)
 imgs = 0
 
 while True:
-    try:
-        b, frame = vid_capture.read();
-        
-        cv.imshow("Frame", frame)
+    b, frame = vid_capture.read();
+    
+    cv.imshow("Frame", frame)
 
-        cv.imwrite("imgs\\frame" + str(imgs) + ".jpg", frame)
+    # cv.imwrite("imgs\\frame" + str(imgs) + ".jpg", frame)
 
-        imgs += 1
+    imgs += 1
 
-        key = cv.waitKey(1)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        time.sleep(0.0333) # lessen cpu strain
-    except KeyboardInterrupt:
-        break;
+    # time.sleep(0.0333) # lessen cpu strain
+
+vid_capture.release();
+cv.destroyAllWindows();
