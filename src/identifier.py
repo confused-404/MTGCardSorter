308503ba3fed07b76fb45ed5d39f04e6e75229a5
Yves Kloeckner <hosta.gruen@gmail.com>
2024-03-16 10:14:44 -0600

    updated regex, further image processing, still ocr problems

--- src/identifier.py
+++ src/identifier.py
@@ -2,19 +2,22 @@
 
 import pytesseract
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageFilter
 import re
 import os
 
 # returns name, set, collector number of card from image containing card using OCR
 # in the form of a dictionary
 def getCardName(img_path):
-    setcodepattern = r"(?<=\s)[A-Z]{2,}[A-Z1-9]{1}(?=\s*[!-/:-@[-`{-~+_©]|\s*\+\s*)|(?<=\s)[M]\d+(?=\s+)|(?<=\s)[A-Z]{3}(?=\s+[+])"
-    collectorpattern = r"(?<=[URCMLurcml¢€]\s)\d+(?=\s+)|(?<=\s)\d+(?=\s+[€URCMLurcml¢]\s+[a-zA-Z])|(?<=\s)\d{2,}(?=\/\d+\s+[€URCMLurcml¢])|(?<=\s)\d+(?=\s[!@#$%^&*_+]+)"
+    setcodepattern = r"(?<=\s)[A-Z]{2,}[A-Z1-9]{1}(?=\s*[!-/:-@[-`{-~+_©«]|\s*\+\s*)|(?<=\s)[M]\d+(?=\s+|\+)|(?<=\s)[A-Z]{3}(?=\s+[+])"
+    collectorpattern = r"\d+(?=\/\d+\s[UCMRLucmrl])|\d+(?=\s+[URCMLucmrl])|(?<=[UCMRLucmrl]\s)\d+(?=\s+[A-Za-z]{3,})|(?<=\s)\d+(?=\s+[!@#$%^&*()]\s+[A-Z]{3,})"
     
     pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+    
     img = Image.open(img_path)
-    img = img.crop((0, img.height/2, img.width / 2, img.height)).convert("L")
+    img = img.crop((0, img.height/1.1, img.width / 2, img.height)).convert("L")
+    img = img.resize((img.width * 5, img.height*5), resample=Image.BOX)
+    img = img.filter(ImageFilter.BLUR)
     
     if not (os.path.isfile("imgs/cropped/" + img_path.split("/")[-1].split(".")[0] + "_cropped.jpg")):
             img.save("imgs/cropped/" + img_path.split("/")[-1].split(".")[0] + "_cropped.jpg")
@@ -24,7 +27,7 @@ def getCardName(img_path):
     
     raw = pytesseract.image_to_string(arr)
     
-    # print("\n\n" + raw + "\n\n")
+    print("\n\n" + raw + "\n\n")
     
     setcode = ""
     collector = -1
@@ -33,7 +36,6 @@ def getCardName(img_path):
         setcode = re.findall(pattern=setcodepattern, string=raw)[0]
     except:
         print("Set Code FAILURE")
-        
     try:
         collector = re.findall(pattern=collectorpattern, string=raw)[0]
         # print("Collector Number: " + collector)
