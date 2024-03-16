24ba923b0b6d1f9606bfbde68a084f6e2b65c55a
Yves Kloeckner <hosta.gruen@gmail.com>
2024-03-16 15:45:09 -0600

    split id into setcode and name

--- src/identifier.py
+++ src/identifier.py
@@ -9,9 +9,23 @@ import os
 # returns name, set, collector number of card from image containing card using OCR
 # in the form of a dictionary
 def getCardName(img_path):
-    setcodepattern = r"(?<=\s)[A-Z]{2,}[A-Z1-9]{1}(?=\s*[!-/:-@[-`{-~+_©«]|\s*\+\s*)|(?<=\s)[M]\d+(?=\s+|\+)|(?<=\s)[A-Z]{3}(?=\s+[+])"
-    collectorpattern = r"\d+(?=\/\d+\s[UCMRLucmrl])|\d+(?=\s+[URCMLucmrl])|(?<=[UCMRLucmrl]\s)\d+(?=\s+[A-Za-z]{3,})|(?<=\s)\d+(?=\s+[!@#$%^&*()]\s+[A-Z]{3,})"
+    pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
     
+    raw_img = Image.open(img_path)
+    input_name = img_path.split("/")[-1].split(".")[0]
+    
+    name_img = raw_img.crop((raw_img.width/15, raw_img.height/20, raw_img.width/1.3, raw_img.height / 10))
+    # name_img = name_img.resize((name_img.width*10, name_img.height*10), resample=Image.BOX)
+    # name_img = name_img.filter(ImageFilter.BLUR)
+    name_img.save("imgs/cropped/name/" + input_name + ".jpg")
+    
+    card_name = pytesseract.image_to_string(name_img)
+    card_name = re.sub(string=card_name, pattern=r"[^\w\s]", repl="")
+    card_name = card_name.strip()
+    
+    return card_name
+    
+def getSetCode(img_path):
     pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
     
     raw_img = Image.open(img_path)
@@ -26,16 +40,6 @@ def getCardName(img_path):
     set_code = re.sub(string=set_code, pattern=r"[^[A-Z]]", repl="")
     set_code = set_code.strip()
     
-    name_img = raw_img.crop((raw_img.width/15, raw_img.height/20, raw_img.width/1.3, raw_img.height / 10))
-    # name_img = name_img.resize((name_img.width*10, name_img.height*10), resample=Image.BOX)
-    # name_img = name_img.filter(ImageFilter.BLUR)
-    name_img.save("imgs/cropped/name/" + input_name + ".jpg")
-    
-    card_name = pytesseract.image_to_string(name_img)
-    card_name = re.sub(string=card_name, pattern=r"[^\w\s]", repl="")
-    card_name = card_name.strip()
-    
-    print(set_code)
-    print(card_name)
+    return set_code
 
     
